d2eaccf88e7eb8ef2b9f48ee2f32c189f1b81325
tkaple <146302502+tkaple@users.noreply.github.com>
2024-06-30 16:10:08 -0400

    Update ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -1,36 +1,28 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
 from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
-from consts import INDEX_NAME
+def ingest_docs()->None:
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-
-
-def ingest_docs():
     loader = ReadTheDocsLoader(
-        "langchain-docs/api.python.langchain.com/en/latest/chains"
+        path="langchain-docs/api.python.langchain.com/en/latest/chains", encoding="utf-8"
     )
-
     raw_documents = loader.load()
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     print(f"loaded {len(raw_documents)} documents")
-
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
     documents = text_splitter.split_documents(raw_documents)
     for doc in documents:
         new_url = doc.metadata["source"]
-        new_url = new_url.replace("langchain-docs", "https:/")
+        new_url = new_url.replace("langchain-docs", "https:")
+        new_url = new_url.replace("\\", "//")
         doc.metadata.update({"source": new_url})
-
     print(f"Going to add {len(documents)} to Pinecone")
-    PineconeVectorStore.from_documents(documents, embeddings, index_name=INDEX_NAME)
+    PineconeVectorStore.from_documents(documents, embeddings, index_name="langchain-doc-index1536")
     print("****Loading to vectorstore done ***")
-
+    
 
 if __name__ == "__main__":
     ingest_docs()
