60510e98ffcd5fd01b70f5b13500a4f13d8d16c1
tkaple <146302502+tkaple@users.noreply.github.com>
2024-06-30 16:10:31 -0400

    Update core.py

--- backend/core.py
+++ backend/core.py
@@ -1,35 +1,41 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from typing import Any, Dict, List
-
-from langchain import hub
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.history_aware_retriever import create_history_aware_retriever
-from langchain.chains.retrieval import create_retrieval_chain
+import os
+from typing import Any
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain_community.vectorstores.pinecone import Pinecone as PineconeLangChain
+from pinecone import Pinecone
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
 
-from consts import INDEX_NAME
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 
+INDEX_NAME = "langchain-doc-index1536"
 
-def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
+def run_llm(query: str, chat_history: list[dict[str, Any]] ) -> Any:
     embeddings = OpenAIEmbeddings()
-    docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
-    chat = ChatOpenAI(model_name="gpt-4o", verbose=True, temperature=0)
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-
-    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
-
-    history_aware_retriever = create_history_aware_retriever(
-        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
+    docsearch = PineconeLangChain.from_existing_index(
+        index_name=INDEX_NAME, embedding=embeddings
     )
-
-    combine_docs_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
-    qa = create_retrieval_chain(
-        retriever=history_aware_retriever, combine_docs_chain=combine_docs_chain
+    chat = ChatOpenAI(verbose=True, temperature=0)
+    """
+    This chain first does a retrieval step to fetch relevant documents, then passes those documents into an LLM to generate a response.
+    qa = RetrievalQA.from_chain_type(
+        llm=chat,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True,
     )
+    return qa({"query": query})
+    """
+    """
+    This chain can be used to have conversations with a document. 
+    It takes in a question and (optional) previous conversation history. 
+    If there is a previous conversation history, it uses an LLM to rewrite the conversation into a query to send to a retriever 
+    (otherwise it just uses the newest user input). It then fetches those documents and passes them (along with the conversation) 
+    to an LLM to respond."""
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+    return qa({"question": query, "chat_history": chat_history})
 
-    result = qa.invoke(input={"input": query, "chat_history": chat_history})
-    return result
+
+if __name__ == "__main__":
+    print(run_llm(query="What is LangChain?"))
